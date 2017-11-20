a1e2674de7bc1dbcb84396371aee4f1bb649f4ef
p.cauty <p.cauty@qwant.com>
2017-11-20 17:35:11 +0000

    EXT-143 - Add logs to boards and bookmarks actions

--- js/account.js
+++ js/account.js
@@ -6,6 +6,7 @@ var BOARDS_URL = "//boards.qwant.com";
 var avatar = document.querySelectorAll(".account__avatar")[0];
 var username = document.querySelectorAll(".account__username")[0];
 var boardsLink = document.querySelectorAll(".button__link--board")[0];
+var bookmarksLink = document.querySelectorAll(".button__link--bookmark")[0];
 
 document.querySelectorAll(".account__logout")[0]
     .addEventListener("click", function () {
@@ -27,10 +28,9 @@ boardsLink.addEventListener("click", function () {
     browser.runtime.sendMessage({name: "close-popup"});
 });
 
-document.querySelectorAll(".button__link--bookmark")[0]
-    .addEventListener("click", function () {
-        browser.runtime.sendMessage({name: "close-popup"});
-    });
+bookmarksLink.addEventListener("click", function () {
+    browser.runtime.sendMessage({name: "close-popup"});
+});
 
 document.querySelectorAll(".account__avatar__container")[0]
     .addEventListener("click", function () {
@@ -56,7 +56,7 @@ browser.storage.local.get(["userExtension"], object => {
     object = JSON.parse(object.userExtension);
     avatar.src = PROTOCOLE + object.avatar;
     username.textContent = object.username;
-    boardsLink.href = PROTOCOLE + BOARDS_URL + "/user/" + object.username;
+    boardsLink.href = PROTOCOLE + BOARDS_URL + "/user/" + object.username + '?client=ext-firefox-ol';
 });
 
 browser.runtime.sendMessage({name: "get_login_action"});
