d9d42e10291ff518f60d7d5e89b5fd9053628b02
p.cauty <p.cauty@qwant.com>
2018-04-30 17:16:19 +0100

    EXT-147 - Design team comments

--- js/welcome.js
+++ js/welcome.js
@@ -10,16 +10,6 @@ document.querySelectorAll(".login__input__submit")[0]
         login();
     });
 
-document.querySelectorAll(".button__action--board")[0]
-    .addEventListener("click", function () {
-        login("boards");
-    });
-
-document.querySelectorAll(".button__link--board")[0]
-    .addEventListener("click", function () {
-        login();
-    });
-
 browser.storage.local.get(["userExtension"], object => {
     if(object && object.userExtension) {
         object = JSON.parse(object.userExtension);
