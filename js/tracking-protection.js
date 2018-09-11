f631fe47db878fabf96efcea5348ef9e264ecdc3
p.cauty <p.cauty@qwant.com>
2018-09-11 14:51:14 +0100

    QWANT-1681 Beginning Extension V4

--- js/tracking-protection.js
+++ js/tracking-protection.js
@@ -5,14 +5,7 @@ var textOK = "";
 var textKO = "";
 
 var changeTPIcon = function (state) {
-    var icon = document.querySelectorAll(".tracking-protection__content__icon")[0];
-    if (state === true) {
-        icon.classList.remove("icon__tracking-protection--disabled");
-        icon.classList.add("icon__tracking-protection--enabled");
-    } else {
-        icon.classList.remove("icon__tracking-protection--enabled");
-        icon.classList.add("icon__tracking-protection--disabled");
-    }
+
 };
 
 var changeText = function (state) {
