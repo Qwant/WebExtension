29a2f0ea27750fa38b98e81f56f04305cf987a04
a.magail <a.magail@qwant.com>
2020-10-05 17:08:00 +0200

    EXT-166-Opera

--- js/contentScript.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-chrome.runtime.onMessage.addListener(function (message, sender, callback) {
-    switch (message.name) {
-        case "ping":
-            callback({name: "pong"});
-            break;
-    }
-});
