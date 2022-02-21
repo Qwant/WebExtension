c17124e439ebad52667a881a06114ef67f6c94ed
a.magail <a.magail@qwant.com>
2022-02-21 13:05:25 +0100

    SA-1182 Web Extension 6.1.0 is broken on brave / chrome

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ExtensionRouter from './components/ExtensionRouter'
 import Welcome from './components/Welcome'
-import Msg from './components/Msg'
 
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <ExtensionRouter
-                root={<Welcome />}
-                msg={<Msg />}
-            />
+            <Welcome />
         </div>
     );
 }
